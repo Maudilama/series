c8faf2c10e601ca6efda033cdbdeb46692aae37b
Maud <marieaude.torchy2022@campus-eni.fr>
2023-04-13 16:44:19 +0200

    Utilisation des données et doctrine

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,11 +15,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class SerieController extends AbstractController
 {
     #[Route('', name: 'list')]
-    public function list(): Response
+    public function list(SerieRepository $serieRepository): Response
     {
-        //todo: aller chercher les séries en bdd
-        return $this->render('serie/list.html.twig', [
+        $series = $serieRepository->findBestSeries();
 
+        return $this->render('serie/list.html.twig', [
+            "series" => $series,
         ]);
     }
 
@@ -24,10 +28,13 @@ class SerieController extends AbstractController
      * @Route("/details/{id}", name="details")
      */
 
-    public function details(int $id):Response
+    public function details(int $id, SerieRepository $serieRepository):Response
     {
-        //todo: aller chercher la série en bdd
-        return $this->render('serie/details.html.twig');
+        $serie = $serieRepository->find($id);
+
+        return $this->render('serie/details.html.twig', [
+            "serie" => $serie,
+        ]);
     }
 
 /**
@@ -36,6 +43,48 @@ class SerieController extends AbstractController
 
     public function create():Response
     {
+
+        return $this->render('serie/create.html.twig');
+    }
+
+    /**
+     * @Route("/demo", name="em-demo")
+     */
+    public function demo(EntityManagerInterface $entityManager): Response
+    {
+        //Crée une instance de mon entité
+        $serie = new Serie();
+
+        //hydrater toutes les propriétés
+        $serie->setName('pif');
+        $serie->setBackdrop('dafsd');
+        $serie->setPoster('dafsdf');
+        $serie->setDateCreated(new \DateTime());
+        $serie->setFirstAirDate(new \DateTime("-1 year"));
+        $serie->setLastAirDate(new \DateTime("-6 month"));
+        $serie->setGenres('drama');
+        $serie->setOverview('bla bla bla');
+        $serie->setPopularity(123.00);
+        $serie->setVote(8.2);
+        $serie->setStatus('Canceled');
+        $serie->setTmdbId(329432);
+
+        dump($serie);
+
+        $entityManager->persist($serie);
+        //a ne surtout pas oublier !!
+        $entityManager->flush();
+
+        dump($serie);
+
+        //$entityManager->remove($serie);
+        $serie->setGenres('comedy');
+
+        $entityManager->flush();
+
+
+
+        //$entityManager = $this->getDoctrine()->getManager();
 
         return $this->render('serie/create.html.twig');
     }
